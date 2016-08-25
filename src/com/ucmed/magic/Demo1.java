02d04a97d942724bb73c50ac15530732152118e3
magic <magic@192.168.1.20>
2016-08-25 09:36:37 +0800

    111111111

--- src/com/ucmed/magic/Demo1.java
+++ src/com/ucmed/magic/Demo1.java
@@ -1,9 +1,18 @@
 package com.ucmed.magic;
 
+import java.util.Calendar;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,world");
+		String s = "";
+		Integer i = Calendar.WEEK_OF_MONTH;
+		if (i < 6) {
+			s = "工作日";
+		} else {
+			s = "休息";
+		}
+		System.out.println(s);
 	}
 
 }
